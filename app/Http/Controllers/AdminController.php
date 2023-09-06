86b299309928a0948852a73925f5b5d0c60563d0
rehanulh <rehanulh@gmail.com>
2023-09-06 13:56:46 +0200

    Worked on Logout Functions, Added /Admin/Login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
         return view('admin.index');
+    }
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    }
+
+    public function AdminDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,9 +34,13 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
 });
 
 Route::middleware(['auth', 'role:vendor'])->group(function(){
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
 });
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
 
