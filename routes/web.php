cb44153383cd0c6691a28aa57ab01f26ac79682b
rehanulh <rehanulh@gmail.com>
2023-09-11 19:53:15 +0200

    Frontend Template Design

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\VendorController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
 
 Route::get('/dashboard', function () {
