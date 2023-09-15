ba0948193af8c01a3bdb143ec3005b9a33d0222b
rehanulh <rehanulh@gmail.com>
2023-09-15 04:46:58 +0200

    User Information Change

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,4 +15,32 @@ class UserController extends Controller
         $userData = User::find($id);
         return view('index', compact('userData'));
     }
+
+    public function UserProfileStore( Request $request){
+        $id = Auth::user()->id;
+        $data=User::find($id);
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        
+
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            @unlink(public_path('upload/user_images/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/user_images'),$filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'User Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    
+    }
 }
