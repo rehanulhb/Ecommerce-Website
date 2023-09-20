bc7bb63a9c0823ca2a4fffbdcd4add1315829c79
rehanulh <rehanulh@gmail.com>
2023-09-20 18:56:32 +0200

    Edit Brand and Images with toaster Messages

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -46,5 +46,51 @@ class BrandController extends Controller
             return view('backend.brand.brand_edit', compact('brand'));
     }
 
+    public function UpdateBrand(Request $request){
+
+        $brand_id=$request->id;
+        $old_img=$request->old_image;
+
+        if($request->file('brand_image'))
+        {
+            $image = $request->file('brand_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
+            $save_url = 'upload/brand/'.$name_gen;
+
+            if(file_exists($old_img)){
+                unlink($old_img);
+            }
+    
+            Brand::findOrFail($brand_id)->update([
+                'brand_name' => $request->brand_name,
+                'brand_slug' => strtolower(str_replace(' ', '-',$request->brand_name)),
+                'brand_image' => $save_url, 
+            ]);
+    
+           $notification = array(
+                'message' => 'Brand Updated with Image Successfully',
+                'alert-type' => 'success'
+            );
+    
+            return redirect()->route('all.brand')->with($notification); 
+        }
+
+        else{
+                Brand::findOrFail($brand_id)->update([
+                'brand_name' => $request->brand_name,
+                'brand_slug' => strtolower(str_replace(' ', '-',$request->brand_name)),
+                 
+            ]);
+    
+           $notification = array(
+                'message' => 'Brand Updated without Image Successfully',
+                'alert-type' => 'success'
+            );
+    
+            return redirect()->route('all.brand')->with($notification);
+        }
+    }
+
 
 }
