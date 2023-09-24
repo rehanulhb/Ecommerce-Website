c7f5bb2e6cd8d3a3b5729c3bf7c81ba565d6f790
rehanulh <rehanulh@gmail.com>
2023-09-25 01:34:44 +0200

    Subcategory Page, Model, Controller, migrated

--- resources/views/backend/subcategory/subcategory_all.blade.php
+++ resources/views/backend/subcategory/subcategory_all.blade.php
@@ -4,19 +4,19 @@
 <div class="page-content">
 				<!--breadcrumb-->
 				<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-					<div class="breadcrumb-title pe-3">All Category</div>
+					<div class="breadcrumb-title pe-3">All SubCategory</div>
 					<div class="ps-3">
 						<nav aria-label="breadcrumb">
 							<ol class="breadcrumb mb-0 p-0">
 								<li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
 								</li>
-								<li class="breadcrumb-item active" aria-current="page">All Category</li>
+								<li class="breadcrumb-item active" aria-current="page">All SubCategory</li>
 							</ol>
 						</nav>
 					</div>
 					<div class="ms-auto">
 						<div class="btn-group">
-							<a href="{{ route('add.category')}}" class="btn btn-primary" >Add Category</a>
+							<a href="{{ route('add.category')}}" class="btn btn-primary" >Add SubCategory</a>
 							
 							
 							
@@ -34,17 +34,17 @@
 									<tr>
 										<th>Sl</th>
 										<th>Category Name</th>
-										<th>Category Image</th>
+										<th>SubCategory Name</th>
 										<th>Action</th>
 										
 									</tr>
 								</thead>
 								<tbody>
-                                    @foreach($categories as $key => $item)
+                                    @foreach($subcategories as $key => $item)
 									<tr>
 										<td>{{$key+1}}</td>
-										<td>{{ $item->category_name }}</td>
-										<td><img src="{{asset($item->category_image)}}" style="width:70px; height:40px;"></td>
+										<td>{{ $item->category_id }}</td>
+										<td>{{ $item->subcategory_name }}</td>
 										<td>
                                             <a href="{{route('edit.category', $item->id)}}" class="btn btn-info">Edit</a>
                                             <a href="{{route('delete.category', $item->id)}}" class="btn btn-danger" id="delete">Delete</a>
@@ -60,7 +60,7 @@
 									<tr>
                                         <th>Sl</th>
 										<th>Category Name</th>
-										<th>Category Image</th>
+										<th>SubCategory Name</th>
 										<th>Action</th>
 									</tr>
 								</tfoot>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\VendorController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\SubCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -82,6 +83,16 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
 
 Route::controller(CategoryController::class)->group(function(){
     Route::get('/all/category', 'AllCategory')->name('all.category');
+    Route::get('/add/category', 'AddCategory')->name('add.category');
+    Route::post('/store/category', 'StoreCategory')->name('store.category');
+    Route::get('/edit/category/{id}', 'EditCategory')->name('edit.category');
+    Route::post('/update/category', 'UpdateCategory')->name('update.category');
+    Route::get('/delete/category/{id}', 'DeleteCategory')->name('delete.category');
+
+});
+
+Route::controller(SubCategoryController::class)->group(function(){
+    Route::get('/all/subcategory', 'AllSubCategory')->name('all.subcategory');
     Route::get('/add/category', 'AddCategory')->name('add.category');
     Route::post('/store/category', 'StoreCategory')->name('store.category');
     Route::get('/edit/category/{id}', 'EditCategory')->name('edit.category');
