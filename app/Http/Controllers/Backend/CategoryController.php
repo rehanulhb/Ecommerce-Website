16f72693b7ac8121033bcf6fcf8f17614ef6a92f
rehanulh <rehanulh@gmail.com>
2023-09-24 19:47:19 +0200

    Worked on Add category Page and show all the categories

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -14,7 +14,35 @@ class CategoryController extends Controller
     public function AllCategory(){
         $categories = Category::latest()->get();
         return view('backend.category.category_all', compact('categories'));
-}
+    }
+
+
+    public function AddCategory(){
+        return view('backend.category.category_add');
+    }
+
+
+    public function StoreCategory(Request $request){
+
+        $image = $request->file('category_image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(120,120)->save('upload/category/'.$name_gen);
+        $save_url = 'upload/category/'.$name_gen;
+
+        Category::insert([
+            'category_name' => $request->category_name,
+            'category_slug' => strtolower(str_replace(' ', '-',$request->category_slug)),
+            'category_image' => $save_url, 
+        ]);
+
+       $notification = array(
+            'message' => 'Category Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.category')->with($notification); 
+
+    }
 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -82,8 +82,8 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
 
 Route::controller(CategoryController::class)->group(function(){
     Route::get('/all/category', 'AllCategory')->name('all.category');
-    Route::get('/add/category', 'AddBrand')->name('add.category');
-    Route::post('/store/brand', 'StoreBrand')->name('store.brand');
+    Route::get('/add/category', 'AddCategory')->name('add.category');
+    Route::post('/store/category', 'StoreCategory')->name('store.category');
     Route::get('/edit/brand/{id}', 'EditBrand')->name('edit.brand');
     Route::post('/update/brand', 'UpdateBrand')->name('update.brand');
     Route::get('/delete/brand/{id}', 'DeleteBrand')->name('delete.brand');
