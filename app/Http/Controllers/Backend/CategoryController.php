043ddffad32e32cd66cd31ccd2cf8ab43a2c6699
rehanulh <rehanulh@gmail.com>
2023-09-24 17:49:51 +0200

    Created Category Page, Model, Controller

---
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Image;
+
+class CategoryController extends Controller
+{
+    //
+
+    public function AllCategory(){
+        $categories = Category::latest()->get();
+        return view('backend.category.category_all', compact('categories'));
+}
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Backend\BrandController;
+use App\Http\Controllers\Backend\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -77,6 +78,16 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
     Route::post('/update/brand', 'UpdateBrand')->name('update.brand');
     Route::get('/delete/brand/{id}', 'DeleteBrand')->name('delete.brand');
     
+});
+
+Route::controller(CategoryController::class)->group(function(){
+    Route::get('/all/category', 'AllCategory')->name('all.category');
+    Route::get('/add/category', 'AddBrand')->name('add.category');
+    Route::post('/store/brand', 'StoreBrand')->name('store.brand');
+    Route::get('/edit/brand/{id}', 'EditBrand')->name('edit.brand');
+    Route::post('/update/brand', 'UpdateBrand')->name('update.brand');
+    Route::get('/delete/brand/{id}', 'DeleteBrand')->name('delete.brand');
+
 });
 
 
