7fc1c5ac53186e7a768334949e12419f3f50c709
rehanulh <rehanulh@gmail.com>
2023-10-07 03:46:57 +0200

    Worked on Product Delete Functionality

--- routes/web.php
+++ routes/web.php
@@ -134,6 +134,7 @@ Route::controller(ProductController::class)->group(function(){
     Route::get('/product/multiimg/delete/{id}', 'MultiImageDelete')->name('product.multiimg.delete');
     Route::get('/product/inactive/{id}', 'ProductInactive')->name('product.inactive');
     Route::get('/product/active/{id}', 'ProductActive')->name('product.active');
+    Route::get('/delete/product/{id}', 'ProductDelete')->name('delete.product');
     
 });
 
