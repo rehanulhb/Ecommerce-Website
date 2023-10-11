face4fee4051a1fd7e56e01527df32dca8019355
rehanulh <rehanulh@gmail.com>
2023-10-11 19:22:38 +0200

    Created Model, controller and Page for Slider Insert from Backend

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Controllers\Backend\VendorProductController;
+use App\Http\Controllers\Backend\SliderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -159,6 +160,16 @@ Route::controller(ProductController::class)->group(function(){
     Route::get('/product/active/{id}', 'ProductActive')->name('product.active');
     Route::get('/delete/product/{id}', 'ProductDelete')->name('delete.product');
     
+});
+
+Route::controller(SliderController::class)->group(function(){
+    Route::get('/all/slider', 'AllSlider')->name('all.slider');
+    Route::get('/add/category', 'AddCategory')->name('add.category');
+    Route::post('/store/category', 'StoreCategory')->name('store.category');
+    Route::get('/edit/category/{id}', 'EditCategory')->name('edit.category');
+    Route::post('/update/category', 'UpdateCategory')->name('update.category');
+    Route::get('/delete/category/{id}', 'DeleteCategory')->name('delete.category');
+
 });
 
 
