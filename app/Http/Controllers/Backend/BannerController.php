7c780343d5727559ef8738d898558acd1b88911b
rehanulh <rehanulh@gmail.com>
2023-10-13 03:55:36 +0200

    Banner Edit and Delete Done

--- app/Http/Controllers/Backend/BannerController.php
+++ app/Http/Controllers/Backend/BannerController.php
@@ -43,6 +43,77 @@ class BannerController extends Controller
 
     }
 
+    public function EditBanner($id){
+        $banners = Banner::findOrFail($id);
+        return view('backend.banner.banner_edit', compact('banners'));
+    }
+
+    public function UpdateBanner(Request $request){
+
+        $banner_id=$request->id;
+        $old_img=$request->old_img;
+
+        if($request->file('banner_image'))
+        {
+            $image = $request->file('banner_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            Image::make($image)->resize(768,450)->save('upload/banner/'.$name_gen);
+            $save_url = 'upload/banner/'.$name_gen;
+
+            if(file_exists($old_img)){
+                unlink($old_img);
+            }
+    
+            Banner::findOrFail($banner_id)->update([
+                'banner_title' => $request->banner_title,
+                'banner_url' => $request->banner_url,
+                'banner_image' => $save_url,  
+            ]);
+    
+           $notification = array(
+                'message' => 'Banner Updated with Image Successfully',
+                'alert-type' => 'success'
+            );
+    
+            return redirect()->route('all.banner')->with($notification); 
+        }
+
+        else{
+                Banner::findOrFail($banner_id)->update([
+                    'banner_title' => $request->banner_title,
+                    'banner_url' => $request->banner_url,
+                    
+                    
+                 
+            ]);
+    
+           $notification = array(
+                'message' => 'Banner Updated without Image Successfully',
+                'alert-type' => 'success'
+            );
+    
+            return redirect()->route('all.banner')->with($notification);
+        }
+    }
+
+    public function DeleteBanner($id){
+
+        $banners = Banner::findOrFail($id);
+        $img=$banners->banner_image;
+        unlink($img);
+
+        Banner::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Banner Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    
+    }
+
+
     
 
 }
