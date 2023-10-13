c6fb8d37f516e78e88312b40b06392da66a6b9d1
rehanulh <rehanulh@gmail.com>
2023-10-13 03:34:42 +0200

    Banner Add Page and Added to database done

--- app/Http/Controllers/Backend/BannerController.php
+++ app/Http/Controllers/Backend/BannerController.php
@@ -17,6 +17,32 @@ class BannerController extends Controller
         return view('backend.banner.banner_all', compact('banners'));
     }
 
+    public function AddBanner(){
+        return view('backend.banner.banner_add');
+    }
+
+    public function StoreBanner(Request $request){
+
+        $image = $request->file('banner_image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(768,450)->save('upload/banner/'.$name_gen);
+        $save_url = 'upload/banner/'.$name_gen;
+
+        Banner::insert([
+            'banner_title' => $request->banner_title,
+            'banner_url' => $request->banner_url,
+            'banner_image' => $save_url, 
+        ]);
+
+       $notification = array(
+            'message' => 'Banner Inserted Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->route('all.banner')->with($notification); 
+
+    }
+
     
 
 }
--- routes/web.php
+++ routes/web.php
@@ -175,8 +175,8 @@ Route::controller(SliderController::class)->group(function(){
 
 Route::controller(BannerController::class)->group(function(){
     Route::get('/all/banner', 'AllBanner')->name('all.banner');
-    Route::get('/add/slider', 'AddSlider')->name('add.slider');
-    Route::post('/store/slider', 'StoreSlider')->name('store.slider');
+    Route::get('/add/banner', 'AddBanner')->name('add.banner');
+    Route::post('/store/banner', 'StoreBanner')->name('store.banner');
     Route::get('/edit/slider/{id}', 'EditSlider')->name('edit.slider');
     Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
     Route::get('/delete/slider/{id}', 'DeleteSlider')->name('delete.slider');
