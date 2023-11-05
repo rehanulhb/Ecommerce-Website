63ca274406d01d5709ec936862acc9c49b85f938
rehanulh <rehanulh@gmail.com>
2023-11-05 18:42:20 +0100

    Showed category wise Products in Category_view page for individual Categories

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -79,7 +79,7 @@ class IndexController extends Controller
      public function CatWiseProduct(Request $request, $id,$slug){
         $products = Product::where('status',1)->where('category_id',$id)->orderBy('id','DESC')->get();
         $categories = Category::orderBy('category_name','ASC')->get();
-
-        return view('frontend.product.category_view',compact('products','categories'));
+        $breadcat = Category::where('id',$id)->first();
+        return view('frontend.product.category_view',compact('products','categories','breadcat'));
      }
 }
