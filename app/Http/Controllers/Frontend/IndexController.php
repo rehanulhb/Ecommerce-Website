b71b262e25b61ca35aef2ee6b69803f35dc680c8
rehanulh <rehanulh@gmail.com>
2023-11-07 01:58:18 +0100

    worked on Loading sub category wise Product

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -83,4 +83,15 @@ class IndexController extends Controller
         $newProduct = Product::orderBy('id','DESC')->limit(3)->get();
         return view('frontend.product.category_view',compact('products','categories','breadcat','newProduct'));
      }
+
+     public function SubCatWiseProduct(Request $request, $id,$slug){
+        $products = Product::where('status',1)->where('subcategory_id',$id)->orderBy('id','DESC')->get();
+        $categories = Category::orderBy('category_name','ASC')->get();
+        $breadsubcat = SubCategory::where('id',$id)->first();
+        $newProduct = Product::orderBy('id','DESC')->limit(3)->get();
+        return view('frontend.product.subcategory_view',compact('products','categories','breadsubcat','newProduct'));
+     }
+
+
+     
 }
