604e5c65241c9cc4eae3693ec275e4ff322699fa
rehanulh <rehanulh@gmail.com>
2023-11-12 18:02:29 +0100

    Working on Product View using AJAX

--- resources/views/frontend/body/quickview.blade.php
+++ resources/views/frontend/body/quickview.blade.php
@@ -9,7 +9,7 @@
                                 
                                 <!-- MAIN SLIDES -->
                                 
-                                <img src="{{ asset('frontend/assets/imgs/shop/product-16-2.jpg') }}" alt="product image" />
+                                <img src="" alt="product image" id="pimage" />
                                 
                             </div>
                             <!-- End Gallery -->
@@ -17,49 +17,41 @@
                         <div class="col-md-6 col-sm-12 col-xs-12">
                             <div class="detail-info pr-30 pl-30">
                                 
-                                <h5 class="title-detail"><a href="shop-product-right.html" class="text-heading">Seeds of Change Organic Quinoa, Brown</a></h5>
+                                <h5 class="title-detail"><a href="" class="text-heading" id="pname"></a></h5>
                                     <br>
-                                    <div class="attr-detail attr-size mb-30">
+                                    <div class="attr-detail attr-size mb-30" id="sizeArea">
                                         <strong class="mr-10" style="width:60px;">Size : </strong>
-                                        <select class="form-control unicase-form-control" id="size">
-                                            <option selected="" disabled="">--Choose Size--</option>
+                                        <select class="form-control unicase-form-control" id="size" name="size">
                                             
-                                            <option value="Small"> Small</option>
-                                            <option value="Medium"> Medium</option>
-                                            <option value="Large"> Large</option>
                                             
                                         </select>
                                     </div>
 
-                                    <div class="attr-detail attr-size mb-30">
+                                    <div class="attr-detail attr-size mb-30" id="colorArea">
                                         <strong class="mr-10" style="width:60px;">Color : </strong>
-                                        <select class="form-control unicase-form-control" id="size">
-                                            <option selected="" disabled="">--Choose Size--</option>
-                                            
-                                            <option value="Red"> Red</option>
-                                            <option value="Green"> Green</option>
-                                            <option value="Blue"> Blue</option>
+                                        <select class="form-control unicase-form-control" id="color" name="color">
+
                                             
                                         </select>
                                     </div>
 
                                 <div class="clearfix product-price-cover">
                                     <div class="product-price primary-color float-left">
-                                        <span class="current-price text-brand">$38</span>
+                                        <span class="current-price text-brand" id="pprice">$</span>
                                         <span>
-                                            <span class="save-price font-md color3 ml-15">26% Off</span>
-                                            <span class="old-price font-md ml-15">$52</span>
+                                            
+                                            <span class="old-price font-md ml-15" id="oldprice">$</span>
                                         </span>
                                     </div>
                                 </div>
                                 <div class="detail-extralink mb-30">
                                     <div class="detail-qty border radius">
                                         <a href="#" class="qty-down"><i class="fi-rs-angle-small-down"></i></a>
-                                        <input type="text" name="quantity" class="qty-val" value="1" min="1">
+                                        <input type="text" name="qty" id="qty" class="qty-val" value="1" min="1">
                                         <a href="#" class="qty-up"><i class="fi-rs-angle-small-up"></i></a>
                                     </div>
                                     <div class="product-extra-link2">
-                                        <button type="submit" class="button button-add-to-cart"><i class="fi-rs-shopping-cart"></i>Add to cart</button>
+                                        <button type="submit" class="button button-add-to-cart" onclick="addToCart()"><i class="fi-rs-shopping-cart"></i>Add to cart</button>
                                     </div>
                                 </div>
                                 
@@ -68,8 +60,8 @@
                                     <div class="col-md-6">
                                         <div class="font-xs">
                                                 <ul>
-                                                    <li class="mb-5">Brand: <span class="text-brand">Nest</span></li>
-                                                    <li class="mb-5">Category:<span class="text-brand"> Jun 4.2022</span></li>
+                                                    <li class="mb-5">Brand: <span class="text-brand" id="pbrand"></span></li>
+                                                    <li class="mb-5">Category:<span class="text-brand" id="pcategory"> </span></li>
                                                 </ul>
                                         </div>
                                     </div> <!--End coll--> 
@@ -77,8 +69,8 @@
                                     <div class="col-md-6">
                                         <div class="font-xs">
                                                 <ul>
-                                                    <li class="mb-5">Product Code: <span class="text-brand">Nest</span></li>
-                                                    <li class="mb-5">Stock:<span class="badge badge-pill badge-success" id="available" style="background:green; color:white;"> Jun 4.2022</span><span class="badge badge-pill badge-danger" id="stockout" style="background:red; color:white;"> Jun 4.2022</span></li>
+                                                    <li class="mb-5">Product Code: <span class="text-brand" id="pcode"></span></li>
+                                                    <li class="mb-5">Stock:<span class="badge badge-pill badge-success" id="available" style="background:green; color:white;"> </span><span class="badge badge-pill badge-danger" id="stockout" style="background:red; color:white;"> </span></li>
                                                 </ul>
                                         </div>
                                     </div> <!--End coll-->
