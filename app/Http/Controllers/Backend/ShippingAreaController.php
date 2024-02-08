d1a1cd782d13bbe67f466b37f219bb0e37a699d7
rehanulhb <rehanulh@gmail.com>
2024-02-08 20:07:10 +0100

    Working on Shipping Area-Shipdistricts,ShipArea,ShipDivisio

--- app/Http/Controllers/Backend/ShippingAreaController.php
+++ app/Http/Controllers/Backend/ShippingAreaController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ShipDistricts;
+use App\Models\ShipDivision;
+use App\Models\ShipState;
+use Carbon\Carbon;
 
 class ShippingAreaController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\CouponController;
+use App\Http\Controllers\Backend\ShippingAreaController;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Controllers\Backend\VendorProductController;
 use App\Http\Controllers\Backend\SliderController;
@@ -193,6 +194,17 @@ Route::controller(BannerController::class)->group(function(){
 
 Route::controller(CouponController::class)->group(function(){
     Route::get('/all/coupon', 'AllCoupon')->name('all.coupon');
+    Route::get('/add/coupon', 'AddCoupon')->name('add.coupon');
+    Route::post('/store/coupon', 'StoreCoupon')->name('store.coupon');
+    Route::get('/edit/coupon/{id}', 'EditCoupon')->name('edit.coupon');
+    Route::post('/update/coupon', 'UpdateCoupon')->name('update.coupon');
+    Route::get('/delete/coupon/{id}', 'DeleteCoupon')->name('delete.coupon');
+
+});
+
+
+Route::controller(ShippingAreaController::class)->group(function(){
+    Route::get('/all/division', 'AllDivision')->name('all.division');
     Route::get('/add/coupon', 'AddCoupon')->name('add.coupon');
     Route::post('/store/coupon', 'StoreCoupon')->name('store.coupon');
     Route::get('/edit/coupon/{id}', 'EditCoupon')->name('edit.coupon');
