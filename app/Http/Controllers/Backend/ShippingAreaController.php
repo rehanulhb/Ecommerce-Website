380ac8c2bd7b81ade658cb29cf1e2d5d25c176db
rehanulhb <rehanulh@gmail.com>
2024-02-08 20:14:19 +0100

    All Division Page Created

--- app/Http/Controllers/Backend/ShippingAreaController.php
+++ app/Http/Controllers/Backend/ShippingAreaController.php
@@ -12,4 +12,8 @@ use Carbon\Carbon;
 class ShippingAreaController extends Controller
 {
     //
+    public function AllDivision(){
+        $division = ShipDivision::latest()->get();
+        return view('backend.ship.division.division_all', compact('division'));
+    }
 }
